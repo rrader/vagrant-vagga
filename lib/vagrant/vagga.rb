15ddbfdf6113bdc08738895cc39421487f75e52f
Roman Rader <roman.rader@gmail.com>
2015-08-27 09:05:06 +0300

    first commit

---
+++ lib/vagrant/vagga.rb
@@ -0,0 +1,7 @@
+require "vagrant/vagga/version"
+
+module Vagrant
+  module Vagga
+    # Your code goes here...
+  end
+end
---
+++ lib/vagrant/vagga/version.rb
@@ -0,0 +1,5 @@
+module Vagrant
+  module Vagga
+    VERSION = "0.1.0"
+  end
+end
