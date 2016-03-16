19cb2b21f0357a796cb380bc34fbaf8cd968699b
Roman Rader <roman.rader@gmail.com>
2016-03-16 20:59:37 +0200

    install uidmap on 12.04; closes #7

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,13 @@
+ENV['VAGRANT_DEFAULT_PROVIDER'] = 'virtualbox'
 Vagrant.configure(2) do |config|
+  #config.vm.box = "hashicorp/precise64"
   config.vm.box = "ubuntu/trusty64"
+
   config.vagga.install = true
+  config.vagga.testing = false
 
   config.vm.provision :vagga
+  if Vagrant.has_plugin?("vagrant-vbguest")
+    config.vbguest.auto_update = false
+  end
 end
--- lib/vagrant-vagga/setup_vagga.sh
+++ lib/vagrant-vagga/setup_vagga.sh
@@ -6,7 +6,12 @@ REPO=$1
 
 if [ -f /etc/lsb-release ]; then
     # install uidmap in Ubuntu
-    apt-get -qq install -y --force-yes uidmap
+    if ! apt-get -qq install -y --force-yes uidmap
+    then
+        # install uidmap manually on ubuntu 12.04
+        wget http://gr.archive.ubuntu.com/ubuntu/pool/main/s/shadow/uidmap_4.1.5.1-1ubuntu9_amd64.deb
+        dpkg -i uidmap_4.1.5.1-1ubuntu9_amd64.deb
+    fi
 fi
 
 echo "deb http://ubuntu.zerogw.com $REPO main" | sudo tee /etc/apt/sources.list.d/vagga.list
@@ -15,6 +20,9 @@ apt-get update
 apt-get install vagga -y --force-yes
 
 mkdir -p ~vagrant/.vagga
+
+echo "vagrant:100000:65536" > /etc/subuid
+echo "vagrant:100000:65536" > /etc/subgid
 
 cat > ~vagrant/.vagga/settings.yaml << EOF
 storage-dir: "/home/vagrant/"
